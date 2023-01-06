cc1fead37006d2307c4ee2eb5498c40f75c08fc5
henrique11varela <henrique11varela@gmail.com>
2023-01-06 23:15:13 +0000

    primeira aula

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+let openMenu = function() {
+    let ham = document.getElementById("iham");
+    let cross = document.getElementById("icross");
+    ham.classList.toggle("hidden");
+    cross.classList.toggle("hidden");
+    console.log("hamburger icon changed");
+}
