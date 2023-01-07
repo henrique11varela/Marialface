8ec3f61fb3a72d032cee13e3f470c3373ada522f
henrique11varela <henrique11varela@gmail.com>
2023-01-07 02:11:07 +0000

    added links

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
 let openMenu = function() {
     let ham = document.getElementById("iham");
     let cross = document.getElementById("icross");
+    let menu = document.getElementById("menu");
     ham.classList.toggle("hidden");
     cross.classList.toggle("hidden");
+    menu.classList.toggle("hidden");
     console.log("hamburger icon changed");
 }
