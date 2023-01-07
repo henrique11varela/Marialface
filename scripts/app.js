ec2fa5b36bdc5e6a51a408b4dfd46cef7399fd68
henrique11varela <henrique11varela@gmail.com>
2023-01-07 14:19:11 +0000

    added main pages

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-let openMenu = function() {
+let toggleMenu = function() {
     let ham = document.getElementById("iham");
     let cross = document.getElementById("icross");
     let menu = document.getElementById("menu");
@@ -6,4 +6,9 @@ let openMenu = function() {
     cross.classList.toggle("hidden");
     menu.classList.toggle("hidden");
     console.log("hamburger icon changed");
+}
+
+let changeIframeSrc = function(iframeId, src) {
+    document.getElementById(iframeId).src = src;
+    toggleMenu();
 }
